a6e47683adf8104b83668d0d0acd6023075c8dac
Miracle0312 <prajapatidhani06@gmail.com>
2025-01-09 10:05:42 +0530

    add06

---
+++ CircleArea.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class CircleArea
+{
+    public static void main(String args[])
+    {
+        Scanner scanner= new Scanner(System.in);
+
+        System.out.print("Enter radius of the circle:");
+        double radius = scanner.nextDouble();
+
+        double area = Math.PI * Math.pow(radius,2); 
+
+        System.out.print("Area of the circle:" +area);
+        scanner.close();
+    }
+}
